df351415d9576e46a35bb2a06e795de50faa24de
Patrick Darwinkel <patrick@darwinkel.net>
2024-04-02 11:55:23 +0200

    Calculate summary statistics

--- statistics.py
+++ statistics.py
@@ -15,22 +15,42 @@ def main() -> None:
     print(answer_given)
     print(no_answer)
 
+    answer_given1 = answer_given["Does the question make sense wrt the given context?"].astype(float)
+    no_answer1 = no_answer["Does the question make sense wrt the given context?"].astype(float)
+
+    answer_given2 = answer_given["Does the answer belong to the question?"].astype(float)
+    no_answer2 = no_answer["Does the answer belong to the question?"].astype(float)
+
+    answer_given3 = answer_given["Are the distractors plausible?"].astype(float)
+    no_answer3 = no_answer["Are the distractors plausible?"].astype(float)
+
+    print("\nQuestions (answer given v.s. no answer)")
+    print(answer_given1.describe())
+    print(no_answer1.describe())
     print(
         ttest_rel(
-            answer_given["Does the question make sense wrt the given context?"].astype(float),
-            no_answer["Does the question make sense wrt the given context?"].astype(float),
+            answer_given1,
+            no_answer1,
         )
     )
+
+    print("\nAnswers (answer given v.s. no answer)")
+    print(answer_given2.describe())
+    print(no_answer2.describe())
     print(
         ttest_rel(
-            answer_given["Does the answer belong to the question?"].astype(float),
-            no_answer["Does the answer belong to the question?"].astype(float),
+            answer_given2,
+            no_answer2,
         )
     )
+
+    print("\nDistractors (answer given v.s. no answer)")
+    print(answer_given3.describe())
+    print(no_answer3.describe())
     print(
         ttest_rel(
-            answer_given["Are the distractors plausible?"].astype(float),
-            no_answer["Are the distractors plausible?"].astype(float),
+            answer_given3,
+            no_answer3,
         )
     )
 
