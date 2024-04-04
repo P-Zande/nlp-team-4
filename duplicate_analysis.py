0d9d152d30dcea0441ff7904b3644e5237155018
meerrom <m.romeijnders@student.rug.nl>
2024-04-04 14:51:01 +0200

    Updated duplicate analysis per case

--- duplicate_analysis.py
+++ duplicate_analysis.py
@@ -1,8 +1,16 @@
 import pandas as pd
-from collections import Counter
 
+cases = ["ground_truth", "answer_given", "no_answer"]
 
-def count_duplicates(data):
+# Read the data from the tsv file
+def read_data(filepath):
+    data = pd.read_csv(filepath, sep='\t')
+    newData = data[["case", "answer", "distractor1", "distractor2", "distractor3"]]
+
+    return newData
+
+# count the answer-distractor and distractor-distractor duplicates per case
+def count_duplicates(data, case):
     answerDuplicate = 0
     distractorDuplicate = 0
     countRow = data.shape[0]
@@ -10,35 +18,36 @@ def count_duplicates(data):
     # Turn each dataframe row into a list and loop over the dataframe
     for i in range(countRow):
         rowList = data.loc[i, :].values.flatten().tolist()
-        # Count answer-distractor duplicates
-        if (rowList[0] == rowList[1]) or (rowList[0] == rowList[2]) or (rowList[0] == rowList[3]):
-            answerDuplicate += 1
-        
-        # # Count distractor-distractor duplicates
-        if (rowList[1] == rowList[2]) or (rowList[1] == rowList[3]) or (rowList[2] == rowList[3]):
-            distractorDuplicate += 1
+        if rowList[0] == case:
+            # Count answer-distractor duplicates
+            if (rowList[1] == rowList[2]) or (rowList[1] == rowList[3]) or (rowList[1] == rowList[4]):
+                answerDuplicate += 1
+            
+            # # Count distractor-distractor duplicates
+            if (rowList[2] == rowList[3]) or (rowList[2] == rowList[4]) or (rowList[3] == rowList[4]):
+                distractorDuplicate += 1
     
     return countRow, answerDuplicate, distractorDuplicate
 
-
-# Get the data and extract the answer and distractors
-data = pd.read_csv("datasets/inference_quantitative_analysis.tsv", sep='\t')
-newData = data[["answer", "distractor1", "distractor2", "distractor3"]]
-countRow, answerDuplicate, distractorDuplicate = count_duplicates(newData)
-
 # print the amount of duplicates
-print("Amount of rows in the dataset: " + str(countRow))
-print("Amount of answer duplicates in the distractors: " + str(answerDuplicate))
-print("Amount of distractor duplicates in the distractors: " + str(distractorDuplicate))
-print()
+def print_results(case, row, answer, distractor):
+    print("Case: " + case)
+    print("Amount of rows in the dataset: " + str(row))
+    print("Amount of answer duplicates in the distractors: " + str(answer))
+    print("Amount of distractor duplicates in the distractors: " + str(distractor))
+    print()
 
 
-# Get the data and extract the answer and distractors
-data2 = pd.read_csv("datasets/sheets_qualitative_analysis_paired.tsv", sep='\t')
-newData2 = data2[["answer", "distractor1", "distractor2", "distractor3"]]
-countRow, answerDuplicate, distractorDuplicate = count_duplicates(newData2)
+# Get the data and extract the answer and distractors per case
+data = read_data("datasets/inference_quantitative_analysis.tsv")
 
-# print the amount of duplicates
-print("Amount of rows in the dataset: " + str(countRow))
-print("Amount of answer duplicates in the distractors: " + str(answerDuplicate))
-print("Amount of distractor duplicates in the distractors: " + str(distractorDuplicate))
+for case in cases:
+    countRow, answerDuplicate, distractorDuplicate = count_duplicates(data, case)
+    print_results(case, countRow, answerDuplicate, distractorDuplicate)
+
+# Get the data and extract the answer and distractors per case
+data = read_data("datasets/sheets_qualitative_analysis_paired.tsv") 
+
+for case in cases:
+    countRow, answerDuplicate, distractorDuplicate = count_duplicates(data, case)
+    print_results(case, countRow, answerDuplicate, distractorDuplicate)
